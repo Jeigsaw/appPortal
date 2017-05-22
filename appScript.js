9a681399cb053ae4807411882d0d7e78e82a1cfb
Jeigsaw <sumanull@outlook.com>
2017-05-22 21:35:58 +0545

    adjusted smooth transition when closing errMsg dialog

--- appScript.js
+++ appScript.js
@@ -1,7 +1,21 @@
 // trigger when the submit is clicked
 // first check the input field and
 // store the name of the user
+var uName;
+$("#rtbar button").on("click", function(e){
+    e.preventDefault();
+   var inputUname = $("#loginForm input[name='uName']")[0].value,
+       inputPass = $("#loginForm input[name='passCode']")[0].value;
 
-$("#rtbar button").on("click", "function(){
-    
-}")
+    if(inputUname == '' || inputPass == ''){
+        $("#errMsg").css("opacity","1");
+    } else {
+        uName = inputUname;
+        $("#loginBx").fadeOut('slow');
+    };
+});
+
+// close the errmsg when clicking 'x'
+$("#errMsg a").on("click", function(){
+    $("#errMsg").css("opacity", "0");
+});
